0f0867edaff35ac05d64ac660496d4015334294c
Your Name <you@example.com>
2021-05-18 21:54:21 +0800

    make ok

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     # get request body as text
-    body = request.get_data(as_text=True)+"123"
+    body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     # handle webhook body
     try:
