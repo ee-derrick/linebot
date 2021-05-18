4c7828f275aea4e9bde2a13a9c4d0e3497c205e0
Your Name <you@example.com>
2021-05-18 21:50:57 +0800

    make ok

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, abort
 
+
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -21,6 +23,7 @@ import datetime
 import time
 #======python的函數庫==========
 
+
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
 # Channel Access Token
@@ -28,13 +31,14 @@ line_bot_api = LineBotApi('YcqixOuILYduL5xwR20RHFRW8q9Wgf+Zxd+DjxC2zj2NG5kNEHJTB
 # Channel Secret
 handler = WebhookHandler('21fead04f5a568736230dd83e25ca8cf')
 
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
     # get request body as text
-    body = request.get_data(as_text=True)
+    body = request.get_data(as_text=True)+"123"
     app.logger.info("Request body: " + body)
     # handle webhook body
     try:
@@ -42,6 +46,7 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
+
 
 
 # 處理訊息
@@ -70,7 +75,11 @@ def handle_message(event):
         message = TextSendMessage(text=msg)
         line_bot_api.reply_message(event.reply_token, message)
 
+
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
